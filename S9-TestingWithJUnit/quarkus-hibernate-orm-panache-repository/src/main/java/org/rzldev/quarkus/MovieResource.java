0079609ed226bf946050c0694d96d7d8aa747b1c
rzldev <amrizalrf@gmail.com>
2022-07-11 16:35:45 +0800

    Logging

--- S9-TestingWithJUnit/quarkus-hibernate-orm-panache-repository/src/main/java/org/rzldev/quarkus/MovieResource.java
+++ S9-TestingWithJUnit/quarkus-hibernate-orm-panache-repository/src/main/java/org/rzldev/quarkus/MovieResource.java
@@ -1,5 +1,7 @@
 package org.rzldev.quarkus;
 
+import org.jboss.logging.Logger;
+
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
@@ -13,11 +15,14 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 public class MovieResource {
 
+    private static Logger LOGGER = Logger.getLogger(MovieResource.class);
+
     @Inject
     MovieRepository movieRepository;
 
     @GET
     public Response getAll() {
+        LOGGER.debug("Get all movies inside the database");
         List<Movie> movies = movieRepository.listAll();
         return Response.ok(movies).build();
     }
@@ -27,6 +32,7 @@ public class MovieResource {
     public Response getMovieById(
             @PathParam("id") Long id
     ) {
+        LOGGER.debug("Get the movie by id " + id);
         return movieRepository.findByIdOptional(id)
                 .map(movie -> Response.ok(movie).build())
                 .orElse(Response.status(Response.Status.NOT_FOUND).build());
@@ -37,6 +43,7 @@ public class MovieResource {
     public Response getMovieByTitle(
             @PathParam("title") String title
     ) {
+        LOGGER.debug("Get the movie by the title " + title);
         return movieRepository.find("title", title)
                 .singleResultOptional()
                 .map(movie -> Response.ok(movie).build())
@@ -48,6 +55,7 @@ public class MovieResource {
     public Response getMovieByCountry (
             @PathParam("country") String country
     ) {
+        LOGGER.debug("Get all movies by country " + country);
         List<Movie> movies = movieRepository.findByCountry(country);
 
         if (movies.size() < 1)
@@ -59,11 +67,16 @@ public class MovieResource {
     @POST
     @Transactional
     public Response createMovie(Movie movie) {
+        LOGGER.debug("Crete a new movie inside the database");
         movieRepository.persist(movie);
-        if (movieRepository.isPersistent(movie))
+
+        if (movieRepository.isPersistent(movie)) {
+            LOGGER.info("The movie has been created with id " + movie.getId());
             return Response.created(URI.create("movies/" + movie.getId()))
                     .entity(movie).build();
+        }
 
+        LOGGER.error("Failed to create a movie");
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
 
@@ -73,10 +86,15 @@ public class MovieResource {
     public Response updateMovie(
             @PathParam("id") Long id, Movie newMovie
     ) {
+        LOGGER.debug("Update an existing movie inside the database by id " + id);
         Movie movie = movieRepository.updateMovie(id, newMovie);
-        if (movie == null)
-            return Response.status(Response.Status.NOT_FOUND).build();
 
+        if (movie == null) {
+            LOGGER.error("Failed to update a movie with id " + id);
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        LOGGER.info("The movie has been updated with id " + id);
         return Response.ok(movie).build();
     }
 
@@ -86,9 +104,15 @@ public class MovieResource {
     public Response deleteMovie(
             @PathParam("id") Long id
     ) {
+        LOGGER.debug("Delete an existing movie inside the database by id " + id);
         boolean deleted = movieRepository.deleteById(id);
-        if (!deleted)
+
+        if (!deleted) {
+            LOGGER.error("Failed to delete a movie with id " + id);
             return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        LOGGER.info("The movie has been deleted with id " + id);
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 }
