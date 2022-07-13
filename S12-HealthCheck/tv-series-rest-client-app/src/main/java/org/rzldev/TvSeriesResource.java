833b33605bb3adbdd66fcbfaa347e9d6fecff379
rzldev <amrizalrf@gmail.com>
2022-07-13 10:59:33 +0800

    MicroProfile Metrics

--- S12-HealthCheck/tv-series-rest-client-app/src/main/java/org/rzldev/TvSeriesResource.java
+++ S12-HealthCheck/tv-series-rest-client-app/src/main/java/org/rzldev/TvSeriesResource.java
@@ -3,6 +3,11 @@ package org.rzldev;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
+import org.eclipse.microprofile.metrics.annotation.Metered;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 import org.rzldev.entity.TvSeriesEntity;
@@ -15,15 +20,13 @@ import org.rzldev.repositories.TvSeriesRepository;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Path("/tv-series")
 public class TvSeriesResource {
@@ -54,6 +57,19 @@ public class TvSeriesResource {
     @Path("/fetch")
     @Transactional
     @Produces(MediaType.APPLICATION_JSON)
+    @Counted(
+            name="countFetchTvSeries",
+            description="Count how many times the fetchTvSeries has been invoked."
+    )
+    @Timed(
+            name="timeFetchTvSeries",
+            description="How long it takes to invoke the fetchTvSeries.",
+            unit=MetricUnits.MILLISECONDS
+    )
+    @Metered(
+            name="meteredFetchTvSeries",
+            description="Measures throughput of fetchTvSeries method."
+    )
     public Response fetchTvSeries(
             @QueryParam("title") String title
     ) {
@@ -76,8 +92,39 @@ public class TvSeriesResource {
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
 
+//    @GET
+//    @Path("/{id}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Gauge(
+//            name="gaugeGetTvSeriesById",
+//            description="Time of getByIdTvSeries method.",
+//            unit=MetricUnits.NONE
+//    )
+//    public Response getTvSeriesById(
+//            @PathParam("id") Long id
+//    ) {
+//        Optional<TvSeriesEntity> show = tvSeriesRepository.findByIdOptional(id);
+//
+//        if (show.isPresent()) return Response.ok(show).build();
+//
+//        return Response.status(Response.Status.NOT_FOUND).build();
+//    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Counted(
+            name="countGetAll",
+            description="Count how many times the getAll has been invoked."
+    )
+    @Timed(
+            name="timeGetAll",
+            description="How long it takes to invoke the fetchTvSeries.",
+            unit=MetricUnits.MILLISECONDS
+    )
+    @Metered(
+            name="meteredGetAll",
+            description="Measures throughput of getAll method."
+    )
     public Response getAll(
             @QueryParam("title") String title
     ) {
