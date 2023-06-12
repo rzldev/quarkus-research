c2fbba8246bc47cff3350cc1cbe1e995480372fb
rzldev <amrizalrf@gmail.com>
2023-06-12 19:31:44 +0800

    Moving the section

--- S14-Extras/infinispan/restart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-clear
-docker-compose -f docker-compose-infinispan.yml down 
-docker volume rm $(docker volume ls)
-docker-compose -f docker-compose-infinispan.yml up -d
-#mvn clean quarkus:dev
--- S14-Extras/infinispan/src/main/java/com/github/rzldev/schemas/MessageKey.java
+++
@@ -1,4 +0,0 @@
-package com.github.rzldev.schemas;
-
-public interface MessageKey {
-}
